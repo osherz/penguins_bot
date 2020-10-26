9a06681e9a61084c2b7bf103c615a512c26c95df
Osher Zakin <osherz92@gmail.com>
2020-10-26 10:18:17 +0200

    Move some methods from bot.py to utils.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 from penguin_game import Player, PenguinGroup, Iceberg
 from IcebergsInRisk import *
+from utils import * as utils
 import math
 # import typing
 # from typing import List
@@ -33,11 +34,11 @@ def rescu_icebers_in_risk(game, my_player):
     :type game: Game
     :type my_player: Player
     """
-    icebergs_in_risk = get_icebergs_in_risk(
+    icebergs_in_risk = utlis.get_icebergs_in_risk(
         game, my_player)  # type: IcebergsInRisk
     icebergs_keys_in_risk = sorted(icebergs_in_risk.get_icebergs(
     ), key=lambda iceberg: iceberg.level, reverse=True)  # type: List[Iceberg]
-    icebergs_not_in_risk = get_icebergs_not_in(
+    icebergs_not_in_risk = utlis.get_icebergs_not_in(
         game.get_my_icebergs(), icebergs_in_risk.get_icebergs())  # List[Iceberg]
     for iceberg in icebergs_keys_in_risk:  # type: Iceberg
         # type: IcebergInRisk
@@ -53,51 +54,10 @@ def rescu_icebers_in_risk(game, my_player):
         if len(icebergs_can_to_help) > 0:
             penguins_each_iceberg_send = int(math.ceil(
                 penguins / len(icebergs_can_to_help)))
-            if are_all_has_enough_penguins(icebergs_can_to_help, penguins_each_iceberg_send):
+            if utlis.are_all_has_enough_penguins(icebergs_can_to_help, penguins_each_iceberg_send):
                 print "rescu: ", iceberg
                 send_penguins_groups(icebergs_can_to_help,
                                      penguins_each_iceberg_send, iceberg)
-
-
-def are_all_has_enough_penguins(icebergs, penguins):
-    """
-    Are all icebergs has more penguined that the given
-    :type icebergs: Iceberg
-    :type penguins: int
-    """
-    for iceberg in icebergs:  # type: Iceberg
-        if iceberg.penguin_amount <= penguins:
-            return False
-    return True
-
-
-def get_icebergs_in_risk(game, my_player):
-    """
-    Check which enemy groups put my icebergs in danger.
-    :type game: Game
-    :type myPlayer: Player
-    :return: List[Iceberg]
-    """
-    icbergs_in_risk = IcebergsInRisk()
-    enemy_groups = game.get_enemy_penguin_groups()  # List[Iceberg]
-    for my_iceberg in game.get_my_icebergs():
-        penguins, turns = get_penguins_in_x_turns(game, my_iceberg)
-        if penguins <= 0:
-            icbergs_in_risk.append(my_iceberg, abs(penguins), turns)
-
-    return icbergs_in_risk
-
-
-def get_icebergs_not_in(all_icebergs, icebergs):
-    """
-    Return icebergs that exists in all_icebergs but not in icebergs
-    :type all_icebergs: List[Iceberg]
-    :type icebergs: List[Iceberg]
-    :return: List[Iceberg]
-    """
-    icebergs_to_return = [
-        iceberg for iceberg in all_icebergs if iceberg not in icebergs]
-    return icebergs_to_return
 
 
 def occupy_close_icebergs(game):
@@ -112,100 +72,12 @@ def occupy_close_icebergs(game):
             icebergs, key=my_iceberg.get_turns_till_arrival)
         for dest_iceberg in icebergs:  # type: Iceberg
             turns_to_dest = my_iceberg.get_turns_till_arrival(dest_iceberg)
-            future_pengiuins_in_dest, turns_to_dest = get_penguins_in_x_turns(game,
-                                                                              dest_iceberg, turns_to_dest)
+            future_pengiuins_in_dest, turns_to_dest = utlis.get_penguins_in_x_turns(game,
+                                                                                    dest_iceberg, turns_to_dest)
             if future_pengiuins_in_dest <= 0:
                 penguins_to_send = abs(future_pengiuins_in_dest)+1
                 if my_iceberg.penguin_amount + 1 > penguins_to_send:
                     send_penguins(my_iceberg, penguins_to_send, dest_iceberg)
-
-
-def min_penguins_for_occupy(game, iceberg):
-    """ 
-    Return the minimum of penguins require to occupy iceberg
-    taking in account penguins groups
-
-    :type game: Game
-    :type iceberg: Iceberg
-    :type my_player: Player
-    """
-    min_penguins = iceberg.penguin_amount
-
-    for enemy_group in game.get_enemy_penguin_groups():  # type: PenguinGroup
-        if enemy_group.destination.equal(iceberg):
-            min_penguins += enemy_group.penguin_amount
-
-    for my_group in game.get_my_penguin_groups():  # type: PenguinGroup
-        if my_group.destination.equal(iceberg):
-            min_penguins -= my_group.penguin_amount
-
-    return min_penguins
-
-
-def get_penguins_in_x_turns(game, iceberg, turns=0):
-    """
-    Return how much penguins will be in the given iceberg after x turns, and in howmuch turns.
-    Negative = enemy/neutral penguins,
-    Positive = my player penguins
-    :type game: Game
-    :type iceberg: Iceberg
-    :type turns: int
-    :return: (int, int)
-    """
-    def get_additional_pengions_in_x_turns(iceberg, owner, turns, my_player, enemy):
-        """
-        Return how much penguins will added in x turns, taking in account only this owner
-        """
-        additional_penguins = iceberg.penguins_per_turn * turns
-        if owner.equals(my_player):
-            return additional_penguins
-        elif owner.equals(enemy):
-            return - 1 * additional_penguins
-        return 0
-
-    my_player = game.get_myself()  # type: Player
-    enemy = game.get_enemy()  # type: Player
-
-    group_to_this_iceberg = [
-        group for group in game.get_all_penguin_groups()
-        if group.destination.equals(iceberg)
-        and (turns <= 0 or group.‎turns_till_arrival <= turns)
-    ]
-    group_to_this_iceberg = sorted(group_to_this_iceberg, key=lambda group: group.‎turns_till_arrival)
-
-    penguins = iceberg.penguin_amount
-    owner = iceberg.owner
-    if not owner.equals(my_player):
-        penguins *= -1
-
-    turns_since_start = 0
-    for group in group_to_this_iceberg:  # type: PenguinGroup
-        # Add penguins that the iceberg invreasing
-        penguins += get_additional_pengions_in_x_turns(
-            iceberg, owner, group.turns_till_arrival - turns_since_start, my_player, enemy)
-        turns_since_start += group.turns_till_arrival
-
-        # Add penguins that arrival from outside
-        if group.equals(enemy):
-            penguins -= group.penguin_amount
-        elif group.equals(my_player):
-            penguins += group.penguin_amount
-
-        # update owner
-        if penguins > 0:
-            owner = my_player
-        elif penguins < 0:
-            owner = enemy
-        else:
-            owner = game.get_neutral()
-
-    if turns > turns_since_start:
-        penguins += get_additional_pengions_in_x_turns(
-            iceberg, owner, turns - turns_since_start, my_player, enemy)
-        turns_since_start = turns
-
-    print 'In ', iceberg, ' in ', turns_since_start, ' turns will be ', penguins, ' penguins.'
-    return penguins, turns_since_start
 
 
 def send_penguins(my_iceberg, destination_penguin_amount, destination):
---
+++ utils.py
@@ -0,0 +1,149 @@
+from penguin_game import *
+
+
+def min_penguins_for_occupy(game, iceberg):
+    """ 
+    Return the minimum of penguins require to occupy iceberg
+    taking in account penguins groups
+
+    :type game: Game
+    :type iceberg: Iceberg
+    :type my_player: Player
+    """
+    min_penguins = iceberg.penguin_amount
+
+    for enemy_group in game.get_enemy_penguin_groups():  # type: PenguinGroup
+        if enemy_group.destination.equal(iceberg):
+            min_penguins += enemy_group.penguin_amount
+
+    for my_group in game.get_my_penguin_groups():  # type: PenguinGroup
+        if my_group.destination.equal(iceberg):
+            min_penguins -= my_group.penguin_amount
+
+    return min_penguins
+
+
+def min_penguins_for_occupy(game, source_iceberg, destination_iceberg):
+    """
+    Calculate how much penguins need to be send 
+    so the source_iceberg will occupy the destination_iceberg.
+    Taking in account the level of the destination iceberg, the distance 
+    and the groups that in their way to the destination iceberg.
+
+    :type game: Game
+    :type source_iceberg: Iceberg
+    :type destination_iceberg: Iceberg
+    :rtype: int
+    """
+    distance, min_turns = destination_iceberg.get_turns_till_arrival(
+        source_iceberg)
+    return get_penguins_in_x_turns(game, destination, distance)
+
+
+def get_penguins_in_x_turns(game, iceberg, turns=0):
+    """
+    Return how much penguins will be in the given iceberg after x turns, and in howmuch turns.
+    Negative = enemy/neutral penguins,
+    Positive = my player penguins
+    :type game: Game
+    :type iceberg: Iceberg
+    :type turns: int
+    :rtype: (int, int)
+    :return: (penguins, turns)
+    """
+    my_player = game.get_myself()  # type: Player
+    enemy = game.get_enemy()  # type: Player
+
+    group_to_this_iceberg = [
+        group for group in game.get_all_penguin_groups()
+        if group.destination.equals(iceberg)
+        and (turns <= 0 or group.‎turns_till_arrival <= turns)
+    ]
+    group_to_this_iceberg = sorted(group_to_this_iceberg, key=lambda group: group.‎turns_till_arrival)
+
+    penguins = iceberg.penguin_amount
+    owner = iceberg.owner
+    if not owner.equals(my_player):
+        penguins *= -1
+
+    turns_since_start = 0
+    for group in group_to_this_iceberg:  # type: PenguinGroup
+        # Add penguins that the iceberg invreasing
+        penguins += get_additional_pengions_in_x_turns(
+            iceberg, owner, group.turns_till_arrival - turns_since_start, my_player, enemy)
+        turns_since_start += group.turns_till_arrival
+
+        # Add penguins that arrival from outside
+        if group.equals(enemy):
+            penguins -= group.penguin_amount
+        elif group.equals(my_player):
+            penguins += group.penguin_amount
+
+        # update owner
+        if penguins > 0:
+            owner = my_player
+        elif penguins < 0:
+            owner = enemy
+        else:
+            owner = game.get_neutral()
+
+    if turns > turns_since_start:
+        penguins += get_additional_pengions_in_x_turns(
+            iceberg, owner, turns - turns_since_start, my_player, enemy)
+        turns_since_start = turns
+
+    print 'In ', iceberg, ' in ', turns_since_start, ' turns will be ', penguins, ' penguins.'
+    return penguins, turns_since_start
+
+
+def get_additional_pengions_in_x_turns(iceberg, owner, turns, my_player, enemy):
+    """
+    Return how much penguins will added in x turns, taking in account only this owner
+    """
+    additional_penguins = iceberg.penguins_per_turn * turns
+    if owner.equals(my_player):
+        return additional_penguins
+    elif owner.equals(enemy):
+        return - 1 * additional_penguins
+    return 0
+
+
+def get_icebergs_not_in(all_icebergs, icebergs):
+    """
+    Return icebergs that exists in all_icebergs but not in icebergs
+    :type all_icebergs: List[Iceberg]
+    :type icebergs: List[Iceberg]
+    :rtype: List[Iceberg]
+    """
+    icebergs_to_return = [
+        iceberg for iceberg in all_icebergs if iceberg not in icebergs]
+    return icebergs_to_return
+
+
+def get_icebergs_in_risk(game, my_player):
+    """
+    Check which enemy groups put my icebergs in danger.
+    :type game: Game
+    :type myPlayer: Player
+    :rtype: List[Iceberg]
+    """
+    icbergs_in_risk = IcebergsInRisk()
+    enemy_groups = game.get_enemy_penguin_groups()  # List[Iceberg]
+    for my_iceberg in game.get_my_icebergs():
+        penguins, turns = get_penguins_in_x_turns(game, my_iceberg)
+        if penguins <= 0:
+            icbergs_in_risk.append(my_iceberg, abs(penguins), turns)
+
+    return icbergs_in_risk
+
+
+def are_all_has_enough_penguins(icebergs, penguins):
+    """
+    Are all icebergs has more penguined that the given
+    :type icebergs: Iceberg
+    :type penguins: int
+    """
+    for iceberg in icebergs:  # type: Iceberg
+        if iceberg.penguin_amount <= penguins:
+            return False
+    return True
