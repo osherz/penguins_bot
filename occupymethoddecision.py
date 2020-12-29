e32e25a157d9f5770ed9244fb49b94493d9329e7
Osher Zakin <osherz92@gmail.com>
2020-12-29 11:32:05 +0200

    Add option to support our iceberg even no risk will be

--- occupymethoddecision.py
+++ occupymethoddecision.py
@@ -13,15 +13,18 @@ class OccupyMethodData:
     Handle data that important for occupy.
     """
 
-    def __init__(self, min_penguins_for_occupy, min_penguins_for_neutral, recommended_penguins_for_occupy, method):
+    def __init__(self, min_penguins_for_occupy, min_penguins_for_neutral, recommended_penguins_for_occupy, method,
+                 owner):
         """
         :param method: SEND_PENGUINS or BUILD_BRIDGE
         :type method: str
+        :param owner: The owner of destination if no action will made.
         """
         self.min_penguins_for_occupy = min_penguins_for_occupy
         self.min_penguins_for_neutral = min_penguins_for_neutral
         self.recommended_penguins_for_occupy = recommended_penguins_for_occupy
         self.method = method
+        self.owner = owner
 
 
 class OccupyMethodDecision:
@@ -38,33 +41,43 @@ class OccupyMethodDecision:
         Decide how to occupy the destination iceberg by the source_iceberg
         and how much penguins to use.
         """
-        min_penguins_to_make_neutral, owner = self.__calc_min_penguins_to_send(
+        game = self.__game
+
+        min_penguins_to_make_neutral, owner_if_no_action_will_made = self.__calc_min_penguins_to_send(
             source_iceberg,
             destination_iceberg
         )
         min_penguins_to_send_for_occupy = min_penguins_to_make_neutral + MIN_ADDITIONAL_PENGUINS_FOR_OCCUPY
 
         is_bridge_prefer = False
-        if utils.is_enemy(self.__game, owner):
+        if utils.is_enemy(self.__game, owner_if_no_action_will_made):
+            # Only if the destination iceberg will belong to enemy
+            # we want to think about bridge building.
             is_bridge_prefer, penguins_to_use = self.__is_bridge_prefer(
                 source_iceberg,
                 destination_iceberg,
                 min_penguins_to_send_for_occupy
             )
+        elif utils.is_me(game, owner_if_no_action_will_made):
+            # If the iceberg will belong ot me,
+            # we want to think about sending some support.
+            min_penguins_to_send_for_occupy = __calc_penguins_to_send_for_support(source_iceberg)
 
         if is_bridge_prefer:
             occupy_method_data = OccupyMethodData(
                 penguins_to_use,
                 penguins_to_use,
                 penguins_to_use,
-                BUILD_BRIDGE
+                BUILD_BRIDGE,
+                owner_if_no_action_will_made
             )
         else:
             occupy_method_data = OccupyMethodData(
                 min_penguins_to_send_for_occupy,
                 min_penguins_to_make_neutral,
                 min_penguins_to_send_for_occupy,
-                SEND_PENGUINS
+                SEND_PENGUINS,
+                owner_if_no_action_will_made
             )
 
         return occupy_method_data
@@ -103,3 +116,18 @@ class OccupyMethodDecision:
                 is_bridge_prefer = True
 
         return is_bridge_prefer, penguins_to_use
+
+
+def __calc_penguins_to_send_for_support(source_iceberg):
+    """
+    Calculate how much penguins to send to destination for support
+    when destination belongs me.
+    """
+    if type(source_iceberg) == Iceberg:
+        penguins_per_turn = source_iceberg.penguins_per_turn
+        penguin_amount_to_send = source_iceberg.penguin_amount
+    else:
+        penguins_per_turn = 0
+        penguin_amount_to_send = source_iceberg.penguin_amount - 1
+
+    return min(penguin_amount_to_send, penguins_per_turn)
