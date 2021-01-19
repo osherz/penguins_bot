0052c37cd16f97a5c76c72b7205b046c7c3a2184
Osher Zakin <osherz92@gmail.com>
2021-01-19 18:57:11 +0200

    Update mapchecker to know the new maps

--- mapchecker.py
+++ mapchecker.py
@@ -8,12 +8,17 @@ class MapChecker:
     Class that check in which map we play.
     """
 
-    def __init__(self, game):
+    def __init__(self, game, stay_default=False):
+
         self.__is_2X2_map = False
         self.__is_tricky_map = False
-        self.__is_default_map = True
+        self.__is_extra_far_treasure = False
+        self.__is_extra_far = False
+        self.__is_circles = False
+        self.__is_default_map = stay_default
         self.__is_2020_map = False
-        self.init_maps(game)
+        if not stay_default:
+            self.init_maps(game)
 
         global map_checker
         if map_checker is None:
@@ -24,10 +29,18 @@ class MapChecker:
         Active this function in the first turn of the game only!
         Check in which maps we play.
         """
-        if len(game.get_my_icebergs()) == 2:
+        starter_amount = game.get_my_icebergs()[0].penguin_amount
+        if starter_amount == 17:
             self.__is_2X2_map = True
         elif len(game.get_all_icebergs()) == 5:
             self.__is_tricky_map = True
+        elif starter_amount == 20:
+            self.__is_extra_far = True
+        elif starter_amount == 11:
+            if game.get_neutral_icebergs()[0].penguin_amount == 1:
+                self.__is_circles = True
+            else:
+                self.__is_extra_far_treasure = True
         else:
             level1_20_penguins = [
                 iceberg
@@ -36,9 +49,8 @@ class MapChecker:
             ]
             if any(level1_20_penguins):
                 self.__is_2020_map = True
-
-        if self.__is_2020_map or self.__is_tricky_map or self.__is_2X2_map:
-            self.__is_default_map = False
+            else:
+                self.__is_default_map = True
 
     def is_2X2_map(self):
         return self.__is_2X2_map
@@ -48,6 +60,15 @@ class MapChecker:
 
     def is_tricky_map(self):
         return self.__is_tricky_map
+
+    def is_extra_far(self):
+        return self.__is_extra_far
+
+    def is_extra_far_treasure(self):
+        return self.__is_extra_far_treasure
+
+    def is_circles(self):
+        return self.__is_circles
 
     def is_default_map(self):
         return self.__is_default_map
